728a6125208011914307cda03aa4e9764b829820
Jeferson <mp_bra_nco@hotmail.com>
2021-05-26 15:52:49 -0300

    tetes

--- JP4/Auto_update_form.cs
+++ JP4/Auto_update_form.cs
@@ -100,6 +100,7 @@ namespace JP4
             {
                 version_number[0] = "";
                 version_number[1] = "";
+
                 return version_number;
             }
             // Read the file line by line
